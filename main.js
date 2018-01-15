6ef2c6517f2dcdefbf990243000f85cac8efcb21
Marc-Andre Wessner <marc.andre.w@hotmail.com>
2018-01-15 17:13:43 +0100

    Add responsive text to menu items :8ball:

--- main.js
+++ main.js
@@ -38,6 +38,40 @@ function moveElement(up,element){
   if(!up)element.next().insertBefore(element);
 }
 
+//Get default fontSize
+let defaultFontSize = -1;
+//Calculate width of text
+function calculateNavTextWidth(){
+  console.log("calculating width of text");
+  //Make default font size
+  if(defaultFontSize==-1)
+    defaultFontSize = parseInt($($('ul#menu-topicmenu a').get(0)).css("font-size"));
+  //Fetch the container width
+  let navWidth = $('nav').width();
+  navWidth-=20;
+  //For every element
+  $('ul#menu-topicmenu a').each((index)=>{
+    //Get anchor and clear fontSize
+    let anchor = $($('ul#menu-topicmenu a').get(index));
+    anchor.css("font-size",`${defaultFontSize}px`);
+    let fontSize = defaultFontSize;
+    //Add calculator class
+    anchor.addClass("calculate-width");
+    while(anchor.width()>navWidth){
+      anchor.css("font-size",`${fontSize--}px`);
+    }
+    anchor.removeClass("calculate-width");
+  });
+}
+function resetCalculatedNavTextWidth(){
+  //For every element
+  $('ul#menu-topicmenu a').each((index)=>{
+    //Get anchor and clear fontSize
+    let anchor = $($('ul#menu-topicmenu a').get(index));
+    anchor.css("font-size",`${defaultFontSize}px`);
+  });
+}
+
 //Define the resize function
 var onresize = (event)=>{
   var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
@@ -45,6 +79,8 @@ var onresize = (event)=>{
   var divAside = $('.aside-holder>div');
   //Check if for resizes and stuff -- Desktop mode
   if(width>900){
+    //Reset calculated text width
+    resetCalculatedNavTextWidth();
     if(divAside.hasClass('flexmode'))divAside.removeClass('flexmode');
     if(recentPosts.hasClass('ontop')){
       moveElement(false,recentPosts);
@@ -52,6 +88,8 @@ var onresize = (event)=>{
     }
   //Mobile mode
   }else{
+    //on mobile calculate width of text
+    calculateNavTextWidth();
     if(!divAside.hasClass('flexmode'))divAside.addClass('flexmode');
     if(!recentPosts.hasClass('ontop')){
       moveElement(true,recentPosts);
