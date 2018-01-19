ed5cfdb577cd7418de784c9fe2e620161c62bf93
Marc-Andre Wessner <marc.andre.w@hotmail.com>
2018-01-19 22:21:50 +0100

    Ad the adsense capability to my theme :moneybag:

--- functions.php
+++ functions.php
@@ -55,7 +55,8 @@ function get_recent_posts(){
 
 //Register Customizer img
 function register_customizer($wp_customize){
-  $wp_customize->add_section('niche_section', array('title'=>"Author image", 'active_callback' => 'is_front_page'));
+  //Add the autor section
+  $wp_customize->add_section('niche_author_section', array('title'=>"Author image", 'active_callback' => 'is_front_page'));
   $wp_customize->add_setting(
 		// $id
 		'author_image',
@@ -76,12 +77,47 @@ function register_customizer($wp_customize){
 			// $args
 			array(
 				'settings'		=> 'author_image',
-				'section'		=> 'niche_section',
+				'section'		=> 'niche_author_section',
 				'label'			=> 'Author image',
 				'description'	=> 'Select Author the image'
 			)
 		)
 	);
+  //Add the adsens section
+  $wp_customize->add_section('niche_adsense', array('title'=>"Adsense", 'active_callback' => 'is_front_page'));
+  //Adsense verifier
+  $wp_customize->add_setting('niche_adsense_verifier', array(
+    'default' => esc_html__( 'Script Code', 'x' ),
+    'sanitize_callback' => ''
+  ));
+  $wp_customize->add_control('niche_adsense_verifier', array(
+    'type' => 'textarea',
+    'section' => 'niche_adsense', // Add a default or your own section
+    'label' => "Adsense Verifier",
+    'description' => htmlspecialchars("Ad the adsense code which is going between <head> and </head>"),
+  ));
+  //First adsense block
+  $wp_customize->add_setting('niche_adsense_1', array(
+    'default' => esc_html__( 'Script Code', 'x' ),
+    'sanitize_callback' => ''
+  ));
+  $wp_customize->add_control('niche_adsense_1', array(
+    'type' => 'textarea',
+    'section' => 'niche_adsense', // Add a default or your own section
+    'label' => "Adsense 1",
+    'description' => "Ad the adsense code for the first ad",
+  ));
+  //second adsense block
+  $wp_customize->add_setting('niche_adsense_2', array(
+    'default' => esc_html__( 'Script Code', 'x' ),
+    'sanitize_callback' => ''
+  ));
+  $wp_customize->add_control('niche_adsense_2', array(
+    'type' => 'textarea',
+    'section' => 'niche_adsense', // Add a default or your own section
+    'label' => "Adsense 2",
+    'description' => "Ad the adsense code for the second ad",
+  ));
 }
 add_action('customize_register', 'register_customizer');
 
@@ -94,6 +130,20 @@ function the_terms_conditions_url(){
   ));
   $id = $pages[0]->ID;
   echo get_permalink($id);
+}
+
+
+//Add the "the_content"-hook
+add_filter('the_content', 'adsense_to_post');
+function adsense_to_post($content){
+  if(is_singular('post')){
+    //Fetch ads
+    $adsense1 = get_theme_mod('niche_adsense_1');
+    $adsense2 = get_theme_mod('niche_adsense_2');
+    $content = str_ireplace('%adsense1%', $adsense1, $content);
+    $content = str_ireplace('%adsense2%', $adsense2, $content);
+  }
+  return $content;
 }
 
 
--- index.php
+++ index.php
@@ -5,6 +5,10 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i" rel="stylesheet">
   <meta name="viewport" content="initial-scale=1, maximum-scale=1">
   <title><?php wp_title('|', true, 'right'); ?></title>
+  <?php
+    //Echo the adsense verifier code
+    echo get_theme_mod('niche_adsense_verifier');
+  ?>
 </head>
 <body>
   <?php get_header(); ?>
